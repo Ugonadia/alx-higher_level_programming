c7ac0774160c534cb4cd1a1875ac3e468c252b1f
Ugonadia <uozoeze@gmail.com>
2022-08-23 16:43:09 -0700

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,4 +5,4 @@ def uppercase(str):
         if str_ord > 96 and str_ord < 123:
             str_ord -= 32
         print("{}".format(chr(str_ord)), end='')
-     print(end='\n')
+    print(end='\n')
