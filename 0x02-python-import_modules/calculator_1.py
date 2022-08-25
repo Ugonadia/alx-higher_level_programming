86735c513d167dbe418ac3993ac463342ba41fda
Ugonadia <uozoeze@gmail.com>
2022-08-25 05:51:22 -0700

    task 1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,55 @@
+##!/usr/bin/python3
+
+def add(a, b):
+    """ My addition function
+
+    Args:
+        a = 10
+        b = 5
+
+    Return statement:
+        return a + b
+    """
+
+    return a + b
+
+
+def sub(a, b):
+    """ My subtraction function
+
+    Args:
+        a = 10
+        b = 5
+
+    Return statement:
+        return a - b
+    """
+
+    return a - b
+
+
+def mul(a, b):
+    """ My multiplication function
+
+    Args:
+    a = 10
+    b = 5
+
+    Return statement
+        return a * b
+    """
+
+    return a * b
+
+
+def div(a, b):
+    """ My division function
+
+    Args:
+    a = 10
+    b = 5
+
+    Return statement:
+        return a / b
+    """
+    return a / b
