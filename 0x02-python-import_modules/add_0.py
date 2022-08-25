fb35eaa7b4111ecd4ed00bc6ac01e527b18eedd5
Ugonadia <uozoeze@gmail.com>
2022-08-25 04:30:12 -0700

    task 0

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """ My addition function
+
+    Args:
+        a = 1
+        b = 2
+
+    Return statement:
+        return a + b
+    """
+    return a + b
