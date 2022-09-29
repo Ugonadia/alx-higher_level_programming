ce5b3ec711d8a1d7f67f36572b11ddc7c075cb32
Ugonadia <uozoeze@gmail.com>
2022-09-29 03:38:48 -0700

    task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -119,3 +119,14 @@ class Rectangle(Base):
             int: Rectangle area value
         """
         return self.__width * self.__height
+
+    def display(self):
+        """Prints the rectangle instance with character #
+        """
+        [print("") for _ in range(self.__y)]
+        [print(" " * self.x + "#" * self.width) for _ in range(self.height)]
+
+    def __str__(self):
+        """string representation of Rectangle
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height) 
