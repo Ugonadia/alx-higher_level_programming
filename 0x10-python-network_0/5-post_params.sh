cb7febadd1f341a89e87745cd992da3996ee77e3
Ozoeze Ugo <uozoeze@gmail.com>
2022-12-16 03:24:39 +0000

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Take in URL, send POST request setting some params, display body
-curl -sX POST -d "email=test@gmail.com&subject=I+will+always+be+there+for+PLD" "$1"
+# Script that sends a POST request to the passed URL, and displays the body of the response.
+curl -s "$1" -X POST -d "email=hr%40holbertonschool%2Ecom&subject=I+will+always+be+here+for+PLD"
